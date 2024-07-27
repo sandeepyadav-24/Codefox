01494f5efd086fb8061632966998528a6513cf72
MOH FAIZAN <faizanusmani06@gmail.com>
2024-07-28 00:58:09 +0530

    mcq generator

--- backend/index.ts
+++ backend/index.ts
@@ -48,6 +48,53 @@ app.get("/", (req: Request, res: Response) => {
       run();
 });
 
+
+
+app.get("/mcq", (req: Request, res: Response) => {
+  async function run() {
+      const link = req.query.link;
+      const title = req.query.title;
+      const channel = req.query.channel;
+
+      console.log(link, channel, title)
+      const prompt =  `
+      
+      generate 10 mcq using the content of the video: ${link}
+
+
+      title : ${title}
+    
+      channel: ${channel}`
+    
+      const result = await model.generateContent(prompt);
+      const response = await result.response;
+      const text = response.text();
+      console.log(text);
+
+      const newprompt = text + `
+      
+      ` + `
+      
+      create an mcq json object having below format
+
+      {
+          question:string,
+          option:array,
+          right_answer:index of right answer
+          
+      }`
+
+      const newresult = await model.generateContent(newprompt);
+      const newresponse = await newresult.response;
+      const newtext = newresponse.text();
+      console.log(newtext);
+      res.json(newtext);
+
+    }
+    
+    run();
+});
+
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
